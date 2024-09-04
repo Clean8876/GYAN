d9dc066c5f367f912088a018bfd5150f7150c51b
Clean8876 <120701325+Clean8876@users.noreply.github.com>
2024-09-04 13:27:33 +0530

    cloudinary fixxed

--- server/utils/fileUploader.js
+++ server/utils/fileUploader.js
@@ -1,11 +1,11 @@
-import { v2 as cloudinary} from 'cloudinary'
+import cloudinary from 'cloudinary';
 import dotenv from 'dotenv';
 
 
 
 dotenv.config();
 
-cloudinary.config({
+cloudinary.v2.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUDINARY_KEY,
   api_secret: process.env.CLOUDINARY_SECRET,
